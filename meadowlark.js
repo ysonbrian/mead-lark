66e8261d4adf9180999b8ad7e080e4211dd3cab3
ysonbrian <znlsakxls@gmail.com>
2021-07-05 22:08:37 +0900

    Jest added

--- meadowlark.js
+++ meadowlark.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const expressHandlebars = require('express-handlebars');
 const fortune = require('./lib/fortune');
+const handlers = require('./lib/handlers');
+
 const app = express();
 
 app.engine(
@@ -14,28 +16,43 @@ app.set('view engine', 'handlebars');
 
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.render('home'));
+// app.get('/', (req, res) => res.render('home'));
+app.get('/', handlers.home);
 
-app.get('/about', (req, res) => {
-  res.render('about', { fortune: fortune.getFortune });
-});
+// app.get('/about', (req, res) => {
+//   res.render('about', { fortune: fortune.getFortune });
+// });
+app.get('/about', handlers.about);
 
 app.use(express.static(__dirname + '/public'));
 
-app.use((req, res) => {
-  res.status(404);
-  res.render('404');
-});
+// app.use((req, res) => {
+//   res.status(404);
+//   res.render('404');
+// });
+app.use(handlers.notFound);
 
-app.use((err, req, res, next) => {
-  console.error(err.message);
-  res.status(500);
-  res.render('500');
-});
+// app.use((err, req, res, next) => {
+//   console.error(err.message);
+//   res.status(500);
+//   res.render('500');
+// });
+app.use(handlers.serverError);
 
-app.listen(port, () =>
-  console.log(
-    `Express started on http://localhost:${port}; ` +
-      `press Ctrl-C to terminate.`
-  )
-);
+// app.listen(port, () =>
+//   console.log(
+//     `Express started on http://localhost:${port}; ` +
+//       `press Ctrl-C to terminate.`
+//   )
+// );
+
+if (require.main === module) {
+  app.listen(port, () => {
+    console.log(
+      `Express started on http://localhost:${port}` +
+        '; press Ctrl-C to terminate.'
+    );
+  });
+} else {
+  module.exports = app;
+}
